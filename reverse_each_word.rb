206e9673b726cdc70d1bdd3bfbeb195444b6401d
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-18 00:23:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(sentence)
-  words_array = sentence.split(" ")
+def reverse_each_word(sentence1)
+  words_array = sentence1.split(" ")
   new_array = []
   new_array = words_array.collect {|word| word.reverse }
   new_array.join(" "
